5aa91126498b0f6e9aee82dd302c865234faa02c
acnbc <ana.acnbc@gmail.com>
2025-06-08 16:45:58 -0300

    makefile e header file

---
+++ Makefile
@@ -0,0 +1,22 @@
+NAME = minishell
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -g -lreadline -ltermcap
+
+SRC = 
+OBJ = $(SRC:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) $(OBJ) -o $(NAME) $(CFLAGS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJ)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
---
+++ minishell.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anogueir <anogueir@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/08 16:36:20 by anogueir          #+#    #+#             */
+/*   Updated: 2025/06/08 16:36:25 by anogueir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <dirent.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <readline/readline.h>
+# include <term.h>
+# include <curses.h>    
+
+
+#endif
