03fce34d5cfe50c37955dcda03d3225d25ac7c15
ghoularth <arthur.bouchatong@gmail.com>
2025-06-19 16:19:08 -0300

    primera versao de echo

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,13 +6,23 @@
 /*   By: abouchat <abouchat@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 17:58:37 by abouchat          #+#    #+#             */
-/*   Updated: 2025/06/08 18:03:04 by abouchat         ###   ########.fr       */
+/*   Updated: 2025/06/19 16:16:17 by abouchat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../minishell.h"
 
-char	*echo_func(char *s, int fd)
+int	ft_echo(char **args, int fd, int flag)
 {
-	
+//	int	arg_c;
+//	arg_c = 0;
+	while(*args)
+	{
+//		arg_c++;
+		write(fd, *args, ft_strlen(*args));
+		args++
+	}
+	if (flag = 1)
+		write(fd, "\n", 1);
 }
+
