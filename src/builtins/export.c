0a379cb7c8e2f079353d9e62140e8e11ef60fe61
ghoularth <arthur.bouchatong@gmail.com>
2025-08-20 18:02:41 -0300

    export funcionando, faltando apenas unset e cd

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: abouchat <abouchat@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 16:46:54 by abouchat          #+#    #+#             */
-/*   Updated: 2025/08/19 18:33:16 by abouchat         ###   ########.fr       */
+/*   Updated: 2025/08/20 18:01:22 by abouchat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static char	**first_equal(char *arg)
 	int		i;
 	char	**sep_args;
 
+	if (!arg)
+		return (NULL);
 	sep_args = safe_malloc(sizeof(char **) * 3);
 	i = 0;
 	while (arg[i])
@@ -24,7 +26,7 @@ static char	**first_equal(char *arg)
 		if (arg[i] == '=')
 		{
 			if (i != 0)
-				sep_args[0] = ft_substr(arg, 0, i - 1);
+				sep_args[0] = ft_substr(arg, 0, i);
 			sep_args[1] = ft_substr(arg, i + 1, ft_strlen(arg) - i);
 			break ;
 		}
@@ -33,15 +35,15 @@ static char	**first_equal(char *arg)
 	return (sep_args);
 }
 
-static void	update_args(char *arg_name, char *arg_cont, char *args)
+static void	update_args(char **arg_name, char **arg_cont, char *args)
 {
 	char	**curr;
 
 	curr = first_equal(args);
-	free(arg_name);
-	free(arg_cont);
-	arg_name = curr[0];
-	arg_cont = curr[1];
+	free(*arg_name);
+	free(*arg_cont);
+	*arg_name = curr[0];
+	*arg_cont = curr[1];
 }
 
 int	ft_export(t_env *env_list, char **args)
@@ -52,9 +54,9 @@ int	ft_export(t_env *env_list, char **args)
 
 	arg_name = NULL;
 	arg_cont = NULL;
-	while (++args)
+	while (++args && *args)
 	{
-		update_args(arg_name, arg_cont, *args);
+		update_args(&arg_name, &arg_cont, *args);
 		if (arg_name == NULL)
 			write(2, "export: not a valid identifier\n", 32);
 		if (find_env_var(arg_name, env_list))
