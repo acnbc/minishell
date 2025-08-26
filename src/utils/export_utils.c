fcba619047f453c69e5249aa255b41f1b539f955
ghoularth <arthur.bouchatong@gmail.com>
2025-08-26 15:52:09 -0300

    fix: export e unset funcionando corretamente

--- src/utils/export_utils.c
+++ src/utils/export_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anogueir <anogueir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abouchat <abouchat@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 16:13:58 by anogueir          #+#    #+#             */
-/*   Updated: 2025/08/24 16:14:02 by anogueir         ###   ########.fr       */
+/*   Updated: 2025/08/26 15:24:53 by abouchat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 t_env	*find_env_var(char *var, t_env *env_list)
 {
 	t_env	*curr;
+	size_t	len;
 
 	curr = env_list;
+	len = 0;
 	while (curr)
 	{
-		if (!ft_strncmp(var, curr->var_name, ft_strlen(var)))
+		if (ft_strlen(var) >= ft_strlen(curr->var_name))
+			len = ft_strlen(var);
+		else
+			len = ft_strlen(curr->var_name);
+		if (!ft_strncmp(var, curr->var_name, len))
 			break ;
 		curr = curr->next;
 	}
